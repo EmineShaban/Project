a176fc8067a5cceb06de6c731d6a4f664cfa3050
Emine Shaban <emineshaban@icloud.com>
2022-11-18 11:23:50 +0200

    create footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { CoreModule } from "./core/core.module";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +12,35 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { CoreModule } from "./core/core.module";
+// // import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// // import { ThemeListComponent } from './theme-list/theme-list.component';
+// // import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+// import {     HttpClientModule} from '@angular/common/http';
+// // import { MainComponent } from './main/main.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+
+//   ],
+//   imports: [
+//     BrowserModule,
+//     CoreModule,
+//     HttpClientModule,
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
