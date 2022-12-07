27b791466f89df48862f29d2494f5bdae6a91fcf
Emine Shaban <emineshaban@icloud.com>
2022-12-07 21:56:59 +0200

    all meeting css fixed

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -14,29 +14,14 @@ export class HeaderComponent implements OnInit {
   constructor(private router: Router, private authService: AuthService,) { }
 
   ngOnInit(): void {
-    // console.log(Cookie.check('auth-cookie'))
+
   }
-
   // currentUser$ = this.store.select(globalState => globalState.currentUser);
-  // getCookie(key: string) {
-  //   console.log(this._cookieService.get(key))
-  // }
-  // isLoggedIn$ = this.currentUser$.pipe(map(user => !!user));
-
-  // homePage(path: string): void {
-  //   this.router.navigate([path])
-  // }
-  // charityPage(path: string): void {
-  //   this.router.navigate([path])
-  // }
 
 
   logoutHandler(): void {
     this.authService.logout$().subscribe({
       next: () => {
-        // sessionStorage.removeItem('email');
-        // sessionStorage.removeItem('authToken');
-        // sessionStorage.removeItem('userId');
 
         this.router.navigate(['/']);
       },
