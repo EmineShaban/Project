cb0e9ee1b30fc57efd7d6efcdfce1de8e2b4b04f
Emine Shaban <emineshaban@icloud.com>
2022-12-09 22:31:13 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharityComponent } from './core/charity/charity.component';
 import { HomeComponent } from './core/home/home.component'
-import { AllMeetingsComponent } from './core/meetings/all-meetings/all-meetings.component';
-import { CreateComponent } from './core/meetings/create/create.component'
-import { LoginComponent } from './core/auth/login/login.component'
-import { RegisterComponent } from './core/auth/register/register.component'
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component'
 
 export const routes: Routes = [
@@ -23,11 +19,7 @@ export const routes: Routes = [
       loadChildren: () => import('./core/meetings/meetings.module').then(m => m.MeetingsModule)
    },
    {
-      path: 'create',
-      component: CreateComponent
-   },
-   {
-      path: '',
+      path: 'auth',
       loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule)
   },
    {
--- src/app/core/meetings/meetings-routing.module.ts
+++ src/app/core/meetings/meetings-routing.module.ts
@@ -1,14 +1,19 @@
 import { RouterModule, Routes } from "@angular/router";
 import { AllMeetingsComponent } from "./all-meetings/all-meetings.component";
+import { CreateComponent } from "./create/create.component";
 import { DetailsComponent } from "./details/details.component";
 // import { LoginComponent } from "./login/login.component";
 // import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
     {
-        path: '',
+        path: 'meeting',
         component: AllMeetingsComponent,
     },
+    {
+      path: 'create',
+      component: CreateComponent
+   },
     // {
     //   path: ':meetingId',
     //   component: DetailsComponent,
